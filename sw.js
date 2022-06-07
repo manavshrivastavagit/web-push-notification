0ec502efc33d77e99662fa86fb4e26fe8050f2e8
manavshrivastava <manavshrivastava@hotmail.com>
2022-06-08 01:16:08 +0530

    updated msg

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
-self.addEventListener('push', (options) => {
-  self.registration.showNotification('Hello world!', options);
+self.addEventListener('push', () => {
+  self.registration.showNotification('Vibration Sample', {
+      body: 'Buzz! Buzz!',
+      icon: '../images/touch/chrome-touch-icon-192x192.png',
+      vibrate: [200, 100, 200, 100, 200, 100, 200],
+      tag: 'vibration-sample'
+    });
+  
+  // self.registration.showNotification('Hello world!', options);
 });
--- webpush.js
+++ webpush.js
@@ -3,9 +3,10 @@ const webpush = require("web-push");
 // const vapidKeys = webpush.generateVAPIDKeys();
 // console.log(vapidKeys);
 // VAPID keys should only be generated only once.
-const vapidKeys ={
-  publicKey: 'BP_Pcm60GaSFzk84eYui0_E7NqjZs6B1dr_kvirEc00IByVvv0UYLocsVwdm2bmov6U9EsQN_qxdEMVT3Pmtj-E',
-  privateKey: 'E9HzIBXUSHenAQMDFy2HqQJH4yYK39tWNLD8rGE5EDQ'
+const vapidKeys = {
+  publicKey:
+    "BP_Pcm60GaSFzk84eYui0_E7NqjZs6B1dr_kvirEc00IByVvv0UYLocsVwdm2bmov6U9EsQN_qxdEMVT3Pmtj-E",
+  privateKey: "E9HzIBXUSHenAQMDFy2HqQJH4yYK39tWNLD8rGE5EDQ",
 };
 
 webpush.setVapidDetails(
@@ -17,12 +18,12 @@ webpush.setVapidDetails(
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
   endpoint:
-    "https://fcm.googleapis.com/fcm/send/fVRSWRpA4Us:APA91bF8puDnWTBrTWj6h_952Gyd6B9ajS-BsG-rAyAsRlZ5zafIDbnl5yzx4yU3yzyAP7ID95dOcHsZSufN3ZBk2Ytl6GkV9dxzOwVxj6Azybp3emaCJpIL2J2xJeINtf2-GTFUXD5b",
+    "https://fcm.googleapis.com/fcm/send/cfLlgXNgpZM:APA91bGkK8a5ti2i2xII76NPDodrHSNGSpy__mpjnxTr55EjnYNa8vV_iVY4yxpei75Q9q6i4_AJmSgoeWdNLb7eu7ZzR7S-Mpjx7bQUO8Gls4digp4KwCNJ-BfE4QvhleoTEDdouB3P",
   expirationTime: null,
   keys: {
     p256dh:
-      "BNwwmyq70PstMnlQM2baCGG2NkVP0og0PhDjW_kCRzAMeokz7q9PDYIQepuQZuxqeAfAZv52-wXLm-ED0b-xRdk",
-    auth: "SZnUB6xoTX_yXKv0PA6eXQ",
+      "BETePFJKwwnTDqCMye_BzC1yuWPvHRDnDP_28clQxkBMVkdz5inbWeBuLWz81wLGSONW8AVo_zBJXibs-JXczdI",
+    auth: "6WNkLQvi-dGsQmXoHnutiQ",
   },
 };
 webpush.sendNotification(pushSubscription, "Your Push Payload Text");
